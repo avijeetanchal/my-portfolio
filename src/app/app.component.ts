1aef4e76153958ebc9d29e78d09c18681e71f801
Avijeet Anchal <avijeetanchal>
2024-07-27 17:58:08 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ExperienceComponent } from "./experience/experience.component";
+import { AboutComponent } from "./about/about.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, ExperienceComponent, AboutComponent, ProjectsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'avijeetanchal-portfolio';
-}
+export class AppComponent {}
